9e35e58beffad9a4df22304f627ed4e7726394f0
Shubhangi Mishra <sonalestilo@gmail.com>
2024-10-09 23:56:21 +0530

    Add project files.

---
+++ AspdotnetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspdotnetMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
